b48fa2dede8e288aff3ca899db4b464ed54af31e
Shivam000189 <shivamsharmass9897@gmail.com>
2025-10-25 22:35:28 +0530

    Deleted folder-name from repo

--- frontend/ExpanceTraker/src/App.jsx
+++ frontend/ExpanceTraker/src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Outlet, useNavigate} from "react-router-dom";
 import Signup from './pages/signup';
 import Login from './pages/login';
 import Dashboard from './pages/dashboard';
-import Dashboard1 from "./pages/dashboard1";
 import ProtectedRoute from './components/ProtectedRoute';
 
 
 function App() {
-  
 
   return (
     <Router>
@@ -21,7 +16,6 @@ function App() {
           {/* <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route> */}
           <Route path='/dashboard' element={<ProtectedRoute><Dashboard></Dashboard></ProtectedRoute>}></Route>
         </Routes>
-
     </Router>
   )
 }
